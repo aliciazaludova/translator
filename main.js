de0d3f5dce6ba8432555ed6ee44e4d159e574d32
Alicia Zaludova <aliciazaludova@gmail.com>
2018-03-12 12:34:56 -0500

    added 3 languages in array of obj

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
 console.log('hi');
+
+var languages = [
+    // Spanish
+    {
+        "a": "un",
+        "an": "un",
+        "the": "el",
+        "and": "y",
+        "happy": "feliz",
+        "holiday": "fiesta",
+        "holidays": "fiestas",
+        "new": "nuevo",
+        "year": "ano",
+        "christmas": "navidad",
+        "xmas": "navidad",
+        "season": "temporada",
+        "seasons": "temporadas",
+        "greeting": "saludo",
+        "greetings": "saludos",
+        "hanukkah": "hanukkah",
+        "warm": "calentar",
+        "wishes": "deseos",
+        "kwanzaa": "kwanzaa"
+    },
+    // German
+    {
+        "a": "ein",
+        "an": "ein",
+        "the": "das",
+        "and": "und",
+        "happy": "glücklich",
+        "holiday": "urlaub",
+        "holidays": "ferien",
+        "new": "neu",
+        "year": "jahr",
+        "christmas": "weihnachten",
+        "xmas": "weihnachten",
+        "season": "jahreszeit",
+        "seasons": "jahreszeiten",
+        "greeting": "Gruß",
+        "greetings": "Gruße",
+        "hanukkah": "chanukka",
+        "warm": "warm",
+        "wishes": "wunsch",
+        "kwanzaa": "kwanzaa"
+    },
+    // Klingon
+    {
+        "a": ".",
+        "an": "ghah",
+        "the": ".",
+        "and": "je",
+        "happy": "quch",
+        "holiday": "ql'lop",
+        "holidays": "ql'lop",
+        "new": "chu'",
+        "year": "dis",
+        "christmas": "christmas",
+        "xmas": "christmas",
+        "season": "maqtagh",
+        "seasons": "maqtagh",
+        "greeting": "ghah 'ej duvan",
+        "greetings": "ghah 'ej duvan",
+        "hanukkah": "hanukkah",
+        "warm": "warm",
+        "wishes": "vlneh",
+        "kwanzaa": "kwanzaa"
+    }
+ ];
